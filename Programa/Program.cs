0c83441b6624a3530d877c1c605bf005abda54e2
Melissa Nogales <melissanogales14@gmail.com>
2019-02-24 15:26:21 -0800

    Update Program.cs

--- Programa/Program.cs
+++ Programa/Program.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 
 namespace Programa
 {
-    class C
+    class Paciente
     {
-        
+        private string nombre;
+        private int edad;
+        public string Nombre
+        {
+            get 
+                {
+                    return nombre;
+                }
+            set 
+                {
+                    nombre=value;
+                }
+        }
+        public Paciente(string nombre, int e)
+        {
+            this.nombre=nombre;
+            edad=e;
+        }
+        public void Imprime()
+            {
+                Console.WriteLine(nombre);
+            }
+
+            public void Imprime(int veces)
+            {
+                for (int i=0; i<=veces; i++)
+                Imprime();
+            }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Paciente p =new Paciente("Patricia", 41);
+            List<Paciente> pacientes =new List<Paciente>();
+            pacientes.Add(p);
+            pacientes.Add(new Paciente("Yovanny", 22));
+            p.Imprime(3);
         }
     }
 }
